9fc6dd2ee80dae6108d75190dae51097669e7b18
binoytv9 <binoytv9@gmail.com>
2014-12-19 10:55:00 +0530

    10.scm,9.scm

--- building-abstractions-with-data/10.scm
+++ building-abstractions-with-data/10.scm
@@ -11,8 +11,11 @@
 			       (max p1 p2 p3 p4))))
 
 (define (div-interval x y)
-	(mul-interval x (make-interval (/ 1.0 (upper-bound y))
-				       (/ 1.0 (lower-bound y)))))
+	(if (and (= upper-bound 0) (= lower-bound 0))
+		(error "second interval spans to zero!!!")
+		(else
+			(mul-interval x (make-interval (/ 1.0 (upper-bound y))
+						       (/ 1.0 (lower-bound y)))))))
 
 (define (make-interval a b) (cons a b))
 
